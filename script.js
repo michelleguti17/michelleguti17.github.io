3bb0c962f42f0cd9234ab3ef4d570a86024d5ab1
Use your GitHub username <Enter your email address>
2021-01-09 20:11:41 -0500

    initial command

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let lastModif = new Date(document.lastModified)  
-document.getElementById("lastUpdate").innerHTML = lastModif;
+let d = new Date();
+let currentYear = d.getFullYear();
+document.getElementById("current_year").innerHTML = currentYear;
+
+
+//show to the user the last modified date
+let lastModified = new Date();
+document.getElementById("last_updated").innerHTML = lastModified.toUTCString();
