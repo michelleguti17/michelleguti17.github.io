3eaf4dd2a874ad98eede0386eeb5298353d93fea
Use your GitHub username <Enter your email address>
2021-01-23 16:45:39 -0500

    initial command

---
+++ lesson3/JS/script.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let currentYear = date.getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+
+let lastUpdated = document.lastModified;
+document.getElementById('lastUpdated').textContent = lastUpdated;
