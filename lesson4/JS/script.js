d3d572f97a75150076f1778b4453866bd7c92a33
Use your GitHub username <Enter your email address>
2021-01-30 23:45:54 -0500

    initial command

--- lesson4/JS/script.js
+++ lesson4/JS/script.js
@@ -1,12 +1,13 @@
-let date = new Date();
-let currentYear = date.getFullYear();
-document.getElementById("currentYear").textContent = currentYear;
-
-
-let lastUpdated = document.lastModified;
-document.getElementById('lastUpdated').textContent = lastUpdated;
-
 function toggleMenu(){
   
-    document.getElementById("primaryNav").classList.toggle("hide");
-  }
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentDate = new Date()
+let actualDate = weekdays[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear()
+document.getElementById("date").innerHTML = actualDate;
+
+
--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById("currentYear").textContent = currentYear;
 
 let lastUpdated = document.lastModified;
 document.getElementById('lastUpdated').textContent = lastUpdated;
+
+
+const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentDate = new Date()
+let actualDate = weekdays[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear()
+document.getElementById('current-date').textContent = formattedDate;
