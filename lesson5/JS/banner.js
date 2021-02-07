976d6b4945413645df852f5f3a8ca9e548812493
Use your GitHub username <Enter your email address>
2021-02-06 20:48:21 -0500

    initial command

---
+++ lesson5/JS/banner.js
@@ -0,0 +1,6 @@
+let bannerDate = new Date();
+let dayOfWeek = bannerDate.getDay();
+let pancake = document.getElementById('pancake-day');
+if (dayOfWeek == 5) {
+    element.style.display = "block";
+}
---
+++ lesson5/JS/script.js
@@ -0,0 +1,12 @@
+function toggleMenu(){
+  
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentDate = new Date()
+let actualDate = weekdays[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear()
+document.getElementById("date").innerHTML = actualDate;
+
+
