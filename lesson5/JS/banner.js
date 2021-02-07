bb51b28bcfb3a9b7388ef1c1076593e241cb74fd
Use your GitHub username <Enter your email address>
2021-02-06 22:06:42 -0500

    initial command

--- lesson5/JS/banner.js
+++
@@ -1,6 +0,0 @@
-const completeDate = new Date();
-let today = completeDate.getDay();
-let element = document.getElementById('friday-banner');
-if (today == 5) {
-    element.style.display = "block";
-}
--- lesson5/JS/script.js
+++ lesson5/JS/script.js
@@ -3,10 +3,17 @@ function toggleMenu(){
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+
 const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let currentDate = new Date()
+const currentDate = new Date()
 let actualDate = weekdays[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear()
 document.getElementById("date").innerHTML = actualDate;
 
 
+
+let weekDay =  currentDate.getDay();
+let element = document.getElementById('friday-banner');
+if (weekDay == 5) {
+    element.style.display = "block";
+}
