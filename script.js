6be953db0792ea7116d8baa09feb8f0262049001
Use your GitHub username <Enter your email address>
2021-03-09 12:03:10 -0500

    initial command

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("currentYear").textContent = currentYear;
 
 let lastUpdated = document.lastModified;
 document.getElementById('lastUpdated').textContent = lastUpdated;
-
+  
 
 const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
