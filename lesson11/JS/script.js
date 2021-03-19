9b8b61a750087f2b7c9702fb8c7eb9e4f965cac8
Use your GitHub username <Enter your email address>
2021-03-19 12:16:17 -0500

    initial command

--- lesson11/JS/script.js
+++ lesson11/JS/script.js
@@ -104,33 +104,12 @@ fetch(requestURL)
       document.querySelector("div.places").append(townName);
       }
       }
-      });
-
-
+      
+     
+  });
 //Wind Chill
 
-
-
-
-
-// Weather Summary PRESTON
-function currentWeather(id) {
-  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id'+id+'&units=imperial&APPID=322f9b768407057c9f7ae572f8cd7a97";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    
-   
-
-    let current = jsObject.main.temp;
-    document.getElementById("current-temp").innerHTML =  Math.round(current);
-    document.getElementById("high-temp").innerHTML = Math.round(jsObject.main.temp_max);
-    document.getElementById("windSpeed").innerHTML = jsObject.wind.speed;
-    document.getElementById("humidity").innerHTML = jsObject.main.humidity;
-    document.getElementById("current-2").innerHTML =  jsObject.weather[0].main;
-   
-    
-    let tempF = parseFloat(document.getElementById ("high-temp").innerHTML);
+let tempF = parseFloat(document.getElementById ("high-temp").innerHTML);
 let speed = parseFloat(document.getElementById ("windSpeed").innerHTML);
 let windchill= windChill(tempF, speed);
 
@@ -150,16 +129,34 @@ else {
 document.getElementById ("windChillOutput").innerHTML= finalResult ;
 console.log (finalResult);
 console.log (windchill);
- 
-   
 
+
+
+// Weather Summary PRESTON
+ function currentWeather(city) {
+  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=" + city + "&units=imperial&appid=322f9b768407057c9f7ae572f8cd7a97";
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+
+    let current = jsObject.main.temp;
+    document.getElementById("current-temp").innerHTML =  Math.round(current);
+    document.getElementById("high-temp").innerHTML = Math.round(jsObject.main.temp_max);
+    document.getElementById("windSpeed").innerHTML = jsObject.wind.speed;
+    document.getElementById("humidity").innerHTML = jsObject.main.humidity;
+    document.getElementById("current-2").innerHTML =  jsObject.weather[0].main;
+   
+    
+    
+ 
   });
+
 }
 // 5 day forecast PRESTON
 
 
-
-const requestURl = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=322f9b768407057c9f7ae572f8cd7a97";
+function forecast(city) {
+const requestURl = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&units=imperial&appid=322f9b768407057c9f7ae572f8cd7a97";
 
 fetch(requestURl)
 .then((response) => response.json())
@@ -184,27 +181,25 @@ fetch(requestURl)
     
  
 });
-
-
+}
 function getTownEvents(town) {
   const eventsURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-fetch(eventsURL)
-.then(function (response) {
-  return response.json();
-})
-.then(function (jsonObject) {
-  const towns = jsonObject['towns'];
-  for (let i = 0; i < towns.length; i++ ) {
-      if (towns[i].name == town) {
-          let events = towns[i].events;
-          for (let i=0; i < events.length; i++) {
-              let event = document.createElement('p');
-              event.innerHTML = events[i];
-              document.querySelector('.events').appendChild(event);
-              console.log(towns);
-          }
+  
+  fetch(eventsURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      const towns = jsonObject['towns'];
+      for (let i = 0; i < towns.length; i++ ) {
+          if (towns[i].name == town) {
+              let events = towns[i].events;
+              for (let i=0; i < events.length; i++) {
+                  let event = document.createElement('p');
+                  event.innerHTML = events[i];
+                  document.querySelector('.events').appendChild(event);
+              }
+        }
       }
-  }
-});
-}
+      });
+    }
