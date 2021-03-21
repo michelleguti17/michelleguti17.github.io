e1717439d4a2d18008a76133a6942b2cc6f49394
Use your GitHub username <Enter your email address>
2021-03-21 10:30:28 -0500

    initial command

--- lesson11/JS/script.js
+++ lesson11/JS/script.js
@@ -6,27 +6,27 @@ const imgOptions ={
     rootMargin: "0px 0px 150px 0px"
 };
 
-const loadImages = (image ) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
   image.onload = () => {
-    image.removeAttribute('data-src');
+    image.removeAttribute("data-src");
   };
 };
-if ('IntersectionObserver' in window) {
+if ("IntersectionObserver" in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
-      loadImages(item.target);
-      observer.unobserve(item.target);
+        loadImages(item.target);
+        observer.unobserve(item.target);
       }
     });
   });
   imagesToLoad.forEach((img) => {
-  observer.observe(img);
+    observer.observe(img);
   });
 } else {
   imagesToLoad.forEach((img) => {
-  loadImages(img);
+    loadImages(img);
   });
 }
 
