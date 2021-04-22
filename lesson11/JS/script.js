b338f8d6ec0b4ec9b493d30a248031f92839ea73
Use your GitHub username <Enter your email address>
2021-04-21 19:09:05 -0500

    INITIAL COMMAND

--- lesson11/JS/script.js
+++ lesson11/JS/script.js
@@ -40,7 +40,7 @@ function toggleMenu(){
 const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDate = new Date()
-let actualDate = weekdays[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear()
+let actualDate = ` ${weekdays[currentDate.getDay()]}, ${currentDate.getDate()}, ${months[currentDate.getMonth()]}, ${currentDate.getFullYear()}`;
 document.getElementById("date").innerHTML = actualDate;
 
 
